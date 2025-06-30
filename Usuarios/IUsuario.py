037f43cd5acbc69b09ddbb5ca8a642491b02e0ce
victorsales1709 <bvictorsales@gmail.com>
2025-06-30 16:14:22 -0300

    Interface usuário

---
+++ Usuarios/IUsuario.py
@@ -0,0 +1,65 @@
+from abc import ABC, abstractmethod
+
+class IUsuario(ABC):
+
+    def __init__(self, id, nome):
+        self._id = id
+        self._nome = nome
+        self._esta_devendo = False
+        self._emprestimos_ativos = []
+        self._reservas_ativas = []
+        self._emprestimos = []
+        self._reservas = []
+    
+    @abstractmethod
+    def get_id(self):
+        pass
+        
+    
+    @abstractmethod
+    def get_nome(self):
+        pass
+      
+    
+    @abstractmethod
+    def get_esta_devendo(self):
+        pass
+
+
+    @abstractmethod
+    def get_emprestimos_ativos(self):
+        pass
+
+    
+    @abstractmethod
+    def get_emprestimos(self):
+        pass
+
+    @abstractmethod
+    def get_reservas_ativas(self):
+        pass
+
+    @abstractmethod
+    def get_reservas(self):
+        pass
+
+    
+    @abstractmethod
+    def mudar_situacao_devedor(self):
+        pass
+        
+    @abstractmethod
+    def adicionar_reserva_ativa(self):
+        pass
+
+    @abstractmethod
+    def adicionar_emprestimo_ativo(self):
+        pass
+
+    @abstractmethod
+    def remover_reserva_ativa(self):
+        pass
+
+    @abstractmethod    
+    def remover_emprestimo_ativo(self):
+        pass
