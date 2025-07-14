5945b93170be9a1cdb3af104e9d3f9740ddcc29b
Otávio <73446599+otavionovais@users.noreply.github.com>
2025-07-14 18:33:52 -0300

    Update Exemplar.py

--- src/Livros/Exemplar.py
+++ src/Livros/Exemplar.py
@@ -1,15 +1,14 @@
 class Exemplar:
-    def __init__(self, id, livro_pai): # <-- 1. Adicionado o parâmetro 'livro_pai'
+    def __init__(self, id, livro_pai): 
         self._id = id
-        self._livro_pai = livro_pai     # <-- 2. Armazenado o objeto Livro
+        self._livro_pai = livro_pai     
         self._disponivel = True
-        self._emprestimo_corrente = None # Necessário para a consulta de livros
-
+        self._emprestimo_corrente = None 
+        
     def get_id(self):
         return self._id
 
     def get_livro(self):
-        """ Retorna o objeto Livro ao qual este exemplar pertence. """
         return self._livro_pai
 
     def esta_disponivel(self):
@@ -19,12 +18,12 @@ class Exemplar:
         if not self._disponivel:
             raise Exception("Exemplar não está disponível.")
         self._disponivel = False
-        self._emprestimo_corrente = emprestimo # Armazena o empréstimo atual
+        self._emprestimo_corrente = emprestimo 
    
 
-    def devolver(self): # Nome alterado para maior clareza
+    def devolver(self): 
         self._disponivel = True
-        self._emprestimo_corrente = None # Limpa a referência ao empréstimo
-
+        self._emprestimo_corrente = None 
+        
     def get_emprestimo_corrente(self):
         return self._emprestimo_corrente
